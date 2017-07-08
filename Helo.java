a009ccb45dbcc70eb8345fa58a3a699a2aec74b7
pavithra-6 <paviikannan1996@gmail.com>
2017-07-08 12:47:43 +0530

    Add files via upload

---
+++ Helo.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class Helo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="Hello";
+String c="world";
+String d=c+a;
+System.out.println(d);
+	}
+
+}
